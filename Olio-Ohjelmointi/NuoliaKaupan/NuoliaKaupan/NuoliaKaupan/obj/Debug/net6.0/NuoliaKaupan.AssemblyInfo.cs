a43a05dcd094709e22fabb584e1f00ea63c07e57
PavenssiSal <s2200897@edu.bc.fi>
2024-01-25 09:25:11 +0200

    Made it work on the new computäh

--- Olio-Ohjelmointi/NuoliaKaupan/NuoliaKaupan/NuoliaKaupan/obj/Debug/net6.0/NuoliaKaupan.AssemblyInfo.cs
+++ Olio-Ohjelmointi/NuoliaKaupan/NuoliaKaupan/NuoliaKaupan/obj/Debug/net6.0/NuoliaKaupan.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NuoliaKaupan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e7e3173768d5160356c082f35edde101ef15407")]
 [assembly: System.Reflection.AssemblyProductAttribute("NuoliaKaupan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NuoliaKaupan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Olio-Ohjelmointi/Ruokatiski/Ruokatiski/Program.cs
+++ Olio-Ohjelmointi/Ruokatiski/Ruokatiski/Program.cs
@@ -2,12 +2,41 @@
 {
     internal class Program
     {
-        enum pääaine { nautaa, kanaa, kasviksia}
-        enum lisuke { riisi, peruna, pasta}
-        enum kastike { curry, hapanimelä, pippuri, chili}
+        enum pääaine { nautaa, kanaa, kasviksia }
+        enum lisuke { riisi, peruna, pasta }
+        enum kastike { curry, hapanimelä, pippuri, chili }
+
+
+        public struct RuokaAnnos
+        {
+            public string Pääraakaaine;
+            public string Liusuke;
+            public string Sauce;
+
+            public RuokaAnnos(string pääraakaaine, string Lisuke, string Kastike, int v)
+            {
+                Pääraakaaine = pääraakaaine;
+                Liusuke = Lisuke;
+                Sauce = Kastike;
+            }
+        }
         static void Main(string[] args)
         {
-            
+
+            Console.WriteLine("Pääaine (nautaa, kanaa, kasviksia)");
+            string pääaine = Console.ReadLine();
+            Console.WriteLine("Lisuke (riisi, peruna, pasta)");
+            string lisuke = Console.ReadLine();
+            Console.WriteLine("Kastike (curry, hapanimelä, pippuri, chili");
+            string sauce = Console.ReadLine();
+
+            RuokaAnnos[] ruokaAnnos = new RuokaAnnos[2];
+            ruokaAnnos[0] = new RuokaAnnos();
+
+            RuokaAnnos[] tyontekijat = new RuokaAnnos[2];
+            tyontekijat[0] = new RuokaAnnos("Suvi", "ohjelmoija", "Yees", 20);
+
+            Console.WriteLine(ruokaAnnos);
         }
     }
 }
