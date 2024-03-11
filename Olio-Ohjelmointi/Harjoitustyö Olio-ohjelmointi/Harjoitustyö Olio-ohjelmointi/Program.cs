f168076e38ab026016c661e18e5687b85f992205
PavenssiSal <s2200897@edu.bc.fi>
2024-03-11 12:55:34 +0200

    Lisäsin kaikille hirviöille vahvuudet ja tein varmaan jotain muutakin jonka unohdin

--- Olio-Ohjelmointi/Harjoitustyö Olio-ohjelmointi/Harjoitustyö Olio-ohjelmointi/Program.cs
+++ Olio-Ohjelmointi/Harjoitustyö Olio-ohjelmointi/Harjoitustyö Olio-ohjelmointi/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
 
 // Luokka hahmolle
 class Knight
@@ -13,6 +16,7 @@ class Knight
     public Potion EquippedPotion { get; set; }
     public int Gold { get; set; }
     public int PotionCount;
+    public bool CanFight = true;
 
     public Knight(string name, int health, int attack, int defense, int gold)
     {
@@ -24,28 +28,79 @@ class Knight
         PotionCount = PotionCount;
     }
 
+
     public void AttackEnemy(Enemy enemy)
     {
+        while (true)
+        {
+            bool IsHit = true;
+
         int damageDealt = Attack - enemy.Defense;
         if (damageDealt < 0)
             damageDealt = 0;
+        //Juttu joka tekee sen että lohikäärmeeseen osumiseen mahdollisuudet on 50/50 jos ei käytä Bowia
+        if (EquippedWeapon != null && EquippedWeapon.Name == "Axe" && enemy.Name == "Dragon" || EquippedWeapon != null && EquippedWeapon.Name == "Sword" && enemy.Name == "Dragon")
+            {
+                // Create an instance of Random class #Random from my good friend GPT
+                Random random = new Random();
 
+                // Generate a random number between 1 and 2
+                int randomNumber = random.Next(1, 3);
+
+                if (randomNumber == 1)
+                {
+                    IsHit = false;
+                }
+            }
+        //Koodi jolla Goblin varastaa yhden pelaajan health potioneista
+        if (PotionCount != 0 && enemy.Name == "Goblin")
+            {
+                Console.WriteLine("The Goblin stole one of your Health Potions");
+                PotionCount--;
+            }
+        //25% mahdollisuus tehdä pelaajasta taiselukyvytön
+        if (enemy.Name == "Orc")
+            {                // Create an instance of Random class #Random from my good friend GPT
+                Random random = new Random();
+
+                // Generate a random number between 1 and 4
+                int randomNumber = random.Next(1, 5);
+
+                if (randomNumber == 1)
+                {
+                    CanFight = false;
+                }
+
+            }
         // Tarkista onko pelaajalla tietty ase ja onko vihollinen hiekkosille
         if (EquippedWeapon != null && EquippedWeapon.Name == "Bow" && enemy.Name == "Dragon")
         {
+                Console.WriteLine("Weapon is very effective against the foe");
             damageDealt += 10; // Lisää 10 vahinkopistettä
         }
         if (EquippedWeapon != null && EquippedWeapon.Name == "Axe" && enemy.Name == "Orc")
         {
-            damageDealt += 10; // Lisää 10 vahinkopistettä
+                Console.WriteLine("Weapon is very effective against the foe");
+                damageDealt += 10; // Lisää 10 vahinkopistettä
         }
         if (EquippedWeapon != null && EquippedWeapon.Name == "Sword" && enemy.Name == "Goblin")
         {
-            damageDealt += 10; // Lisää 10 vahinkopistettä
+                Console.WriteLine("Weapon is very effective against the foe");
+                damageDealt += 10; // Lisää 10 vahinkopistettä
+        }
+        if (IsHit == true)
+            {
+                enemy.Health -= damageDealt;
+                Console.WriteLine($"You attack the {enemy.Name} and deal {damageDealt} damage!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Your attack did not reach the dragon");
+                break;
+            }
         }
 
-        enemy.Health -= damageDealt;
-        Console.WriteLine($"You attack the {enemy.Name} and deal {damageDealt} damage!");
     }
 
     public void DrinkPotion()
@@ -315,33 +370,36 @@ class Program
         Enemy[] enemies = {
             new Enemy("Goblin", 20, 30, 10),
             new Enemy("Orc", 75, 15, 10),
-            //new Enemy("Dragon", 200, 25, 15)
+            new Enemy("Dragon", 100, 25, 15)
         };
 
         Shop shop = new Shop();
 
         Console.WriteLine("Welcome, brave knight! Let the adventure begin!");
 
-        while (true)
+        while (player.Health > 0)
         {
-            
 
-            //UI
-            Console.WriteLine();
+
+            //UI               
+            Console.WriteLine("---------------");
             Console.WriteLine("HP: " + player.Health);
             Console.WriteLine("Gold: " + player.Gold);
             Console.WriteLine("Weapon: " + player.EquippedWeapon.Name);
             Console.WriteLine("Armor: " + player.EquippedArmor.Name);
-            Console.WriteLine("Health potion's: " + player.PotionCount);
+            Console.WriteLine("Health potions remaining: " + player.PotionCount);
             Console.WriteLine("---------------");
             Console.WriteLine("1. Fight an enemy");
             Console.WriteLine("2. Visit the shop");
             Console.WriteLine("3. Quit");
 
+
+
             int choice = Convert.ToInt32(Console.ReadLine());
 
             if (choice == 1)
             {
+                Console.WriteLine("---------------");
                 Enemy enemy = enemies[new Random().Next(0, enemies.Length)];
                 Console.WriteLine("You encounter a " + enemy.Name + "!");
 
@@ -350,42 +408,60 @@ class Program
 
                 while (player.Health > 0 && enemy.Health > 0)
                 {
-                    Console.WriteLine(player.Name + " - Health: " + player.Health + " | Enemy - Health: " + enemy.Health);
-                    Console.WriteLine("1. Attack");
-                    Console.WriteLine("2. Drink Potion");
-
-                    int action = Convert.ToInt32(Console.ReadLine());
-
-                    if (action == 1)
+                    if (player.CanFight == true)
                     {
-                        player.AttackEnemy(enemy);
-                        if (enemy.Health > 0)
+                        Console.WriteLine(player.Name + " - Health: " + player.Health + " | Enemy - Health: " + enemy.Health);
+                        Console.WriteLine("1. Attack");
+                        Console.WriteLine("2. Drink Potion");
+                        Console.WriteLine("---------------");
+
+                        //Please dont f**k up the game and just do as told
+                        int action = Convert.ToInt32(Console.ReadLine());
+
+                        if (action == 1)
+                        {
+                            player.AttackEnemy(enemy);
+                            if (enemy.Health > 0)
+                                enemy.AttackKnight(player);
+                            if (player.CanFight == false)
+                            {
+                                Console.WriteLine("The Orc immobilised you for a turn");
+                            }
+                            Console.WriteLine("---------------");
+                        }
+                        else if (action == 2 && player.PotionCount > 0)
+                        {
+                            player.DrinkPotion();
+                            player.PotionCount--;
                             enemy.AttackKnight(player);
-                    }
-                    else if (action == 2 && player.PotionCount > 0)
-                    {
-                        player.DrinkPotion();
-                        player.PotionCount--;
-                        enemy.AttackKnight(player);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Command failed");
-                    }
+                            Console.WriteLine("---------------");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Command failed");
+                        }
 
-                    if (player.Health <= 0)
-                    {
-                        Console.WriteLine("You have been defeated! Game over.");
-                        break;
+                        if (player.Health <= 0)
+                        {
+                            Console.WriteLine("You have been defeated!");
+                            break;
+                        }
+                        else if (enemy.Health <= 0)
+                        {
+                            int goldEarned = new Random().Next(10, 30);
+                            player.Gold += goldEarned;
+                            Console.WriteLine("You have defeated the " + enemy.Name + " and earned " + goldEarned + " gold!");
+                            break;
+                        }
                     }
-                    else if (enemy.Health <= 0)
+                    if (player.CanFight == false)
                     {
-                        int goldEarned = new Random().Next(10, 30);
-                        player.Gold += goldEarned;
-                        Console.WriteLine("You have defeated the " + enemy.Name +  " and earned " + goldEarned + " gold!");
-                        break;
+                        Console.WriteLine("You are unable to move this turn");
+                        enemy.AttackKnight(player);
+                        player.CanFight = true;
                     }
                 }
+                    
                 // Palauta vihollisen terveys alkuperäiseksi
                 enemy.Health = originalEnemyHealth;
             }
@@ -413,5 +489,6 @@ class Program
                 Console.WriteLine("Invalid choice. Please try again.");
             }
         }
+        Console.WriteLine("Game over");
     }
 }
